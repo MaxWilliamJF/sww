9388c961543d2b7841a0c2aca54ae9fb6816493d
Max G. Damasceno <maxwilljf@gmail.com>
2017-05-22 06:28:45 -0300

    Home page text

--- src/components/shared/Home.js
+++ src/components/shared/Home.js
@@ -5,8 +5,8 @@ class Home extends Component {
     render() {
         return (
             <div id="page" className="width-limit">
-            	<h2>Home page</h2>
-            	<p>Some content goes here</p>
+            	<h2>Welcome to the Star Wars World!</h2>
+            	<p>Here you can find a lot of info about Star Wars, including the films them selves, vehicles in the and of course, about your favorite characters.</p>
             </div>
         );
     }
